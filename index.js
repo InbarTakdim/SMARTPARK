5ca2900084aca2288e12199f3b61287fe60261cf
inbar <inbarytk@gmail.com>
2017-04-26 10:54:27 +0300

    with allow access

--- index.js
+++ index.js
@@ -7,14 +7,14 @@ var bodyParser=require('body-parser');
 var app=express();
 var http= require('http');
 
-/*
+
 app.all('*', function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
 });
-*/
+
 //app.use(bodyParser.json());
 //app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.bodyParser());
